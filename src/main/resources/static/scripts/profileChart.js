3703f5127cc1bb6ca7cf2bffebcaf48684c025c9
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-04-22 23:04:49 +0200

    feat: split charts. fix prettier config

--- src/main/resources/static/scripts/profileChart.js
+++ src/main/resources/static/scripts/profileChart.js
@@ -1,57 +1,60 @@
-window.onload = function(){
+window.onload = function () {
+    console.log("AQUI");
+    console.log(cpu_usage);
+    console.log(io_time);
+    console.log(cpu_time);
 
-    console.log("AQUI")
-    console.log(cpu_usage)
-    console.log(io_time)
-    console.log(cpu_time)
-
-    const ctx = document.getElementById("myChart")
+    const timePercentageCtx = document.getElementById("timePercentageChart");
+    const cpuUsageCtx = document.getElementById("cpuUsageChart");
     try {
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: Array.from({ length: cpu_usage.length }, (value, index) => index),
-        datasets: [{
-            label: "CPU Usage Percentage",
-          data: cpu_usage,
-          borderColor: "red",
-          fill: false
-        },{
+        new Chart(timePercentageCtx, {
+            type: "line",
+            data: {
+                labels: Array.from(
+                    { length: cpu_time.length },
+                    (value, index) => index
+                ),
+                datasets: [
+                    {
+                        label: "I/O Time Percentage",
+                        data: io_time,
+                        borderColor: "green",
+                        fill: false,
+                    },
+                    {
+                        label: "CPU Time Percentage",
+                        data: cpu_time,
+                        borderColor: "blue",
+                        fill: false,
+                    },
+                ],
+            },
+            options: {
+                legend: { display: true },
+            },
+        });
 
-          label: "I/O Time Percentage",
-          data: io_time,
-          borderColor: "green",
-          fill: false
-        },{
-          label: "CPU Time Percentage",
-          data: cpu_time,
-          borderColor: "blue",
-          fill: false
-        }]
-      },
-      options: {
-        legend: {display: true}
-      }
-    });                         
-    }catch(error)
-        { console.log(error)}
-  //  var giro = new Chart(ctx, {
-  //  type: 'bar',
-  //  data: {
-  //    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //    datasets: [{
-  //      label: '# of Votes',
-  //      data: [12, 19, 3, 5, 2, 3],
-  //      borderWidth: 1
-  //    }]
-  //  },
-  //  options: {
-  //    scales: {
-  //      y: {
-  //        beginAtZero: true
-  //      }
-  //    }
-  //  }
-  //});
-    //console.log(giro)
-}
+        new Chart(cpuUsageCtx, {
+            type: "line",
+            data: {
+                labels: Array.from(
+                    { length: cpu_usage.length },
+                    (value, index) => index
+                ),
+                datasets: [
+                    {
+                        label: "CPU Usage Percentage",
+                        data: cpu_usage,
+                        borderColor: "red",
+                        fill: false,
+                    },
+                ],
+            },
+            options: {
+                legend: { display: true },
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
